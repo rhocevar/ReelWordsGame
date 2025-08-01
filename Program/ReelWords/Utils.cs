bf1e4d113f3b5628a7f4fe69a8ae31c05f65a9d4
Rafael Hocevar <hocevar.rafael@gmail.com>
2025-08-01 15:38:13 -0700

    Implemented dictionary read from file into Trie

---
+++ Program/ReelWords/Utils.cs
@@ -0,0 +1,27 @@
+using System.IO;
+
+namespace ReelWords;
+
+public static class Utils
+{
+    public static DirectoryInfo TryGetDirectoryInfo(string folderName)
+    {
+        // Get the current directory where the assembly is being executed
+        string currentDirectoryPath = Directory.GetCurrentDirectory();
+        DirectoryInfo currentDirectory = new DirectoryInfo(currentDirectoryPath);
+        
+        // Traverse up the directory tree until the folder is found
+        while (currentDirectory != null)
+        {
+            DirectoryInfo[] targetDirectory = currentDirectory.GetDirectories(folderName);
+            if (targetDirectory.Length > 0)
+            {
+                return targetDirectory[0];
+            }
+            
+            currentDirectory = currentDirectory.Parent;
+        }
+
+        return null; // No directory was found
+    }
+}
--- Program/ReelWordsTests/TrieTests.cs
+++ Program/ReelWordsTests/TrieTests.cs
@@ -1,28 +1,31 @@
 ﻿using ReelWords;
 using Xunit;
 
-namespace ReelWordsTests
+namespace ReelWordsTests;
+
+public class TrieTests
 {
-    public class TrieTests
+    // TODO:  Create simple unit tests to test your code in the ReelWordsTests project,
+    // don't worry about creating tests for everything, just important functions as
+    // seen for the Trie tests
+    
+    private const string TEST_WORD = "parallel";
+
+    [Fact]
+    public void TrieInsertTest()
     {
-        private const string TEST_WORD = "parallel";
+        Trie trie = new Trie();
+        trie.Insert(TEST_WORD);
+        Assert.True(trie.Search(TEST_WORD));
+    }
 
-        [Fact]
-        public void TrieInsertTest()
-        {
-            Trie trie = new Trie();
-            trie.Insert(TEST_WORD);
-            Assert.True(trie.Search(TEST_WORD));
-        }
-
-        [Fact]
-        public void TrieDeleteTest()
-        {
-            Trie trie = new Trie();
-            trie.Insert(TEST_WORD);
-            Assert.True(trie.Search(TEST_WORD));
-            trie.Delete(TEST_WORD);
-            Assert.False(trie.Search(TEST_WORD));
-        }
+    [Fact]
+    public void TrieDeleteTest()
+    {
+        Trie trie = new Trie();
+        trie.Insert(TEST_WORD);
+        Assert.True(trie.Search(TEST_WORD));
+        trie.Delete(TEST_WORD);
+        Assert.False(trie.Search(TEST_WORD));
     }
 }
